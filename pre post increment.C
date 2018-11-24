c01a0da7c1f9028db0c426274c95f8a2428771ff
Priyanshu132 <45144924+Priyanshu132@users.noreply.github.com>
2018-11-24 20:43:53 +0530

    Add files via upload

---
+++ pre post increment.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+ int a,sum;
+ clrscr();
+ while(a!=0)
+	{
+	   b=a%10;
+	   a=a/10;
+	   sum=sum+pow(b,3)
+	}
+	 if(sum==a)
+	  printf("number is amstorm");
+	getch();
+}
